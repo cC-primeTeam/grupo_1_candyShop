143410b1ff680e5a6ce8e6cfcc54b156b14b2e12
franfernandez <frankie.fcandia@gmail.com>
2020-06-27 17:49:07 -0300

    creamos todas rutas y los styles

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var detalleProductoRouter = require('./routes/detalleProducto');
+let indexRouter = require('./routes/index');
+let detalleProductoRouter = require('./routes/detalleProducto');
+let carritoRouter = require('./routes/carrito');
+let formularioProductoRouter = require('./routes/formularioProducto');
+let registroProductoRouter = require('./routes/registro');
 
-var app = express();
+let usersRouter = require('./routes/users');
+
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +26,12 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 //Rutas
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/detalleProducto', detalleProductoRouter);
+app.use('/carrito', carritoRouter);
+app.use('/formularioProducto', formularioProductoRouter);
+app.use('/registro', registroProductoRouter);
+
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
