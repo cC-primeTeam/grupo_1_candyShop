04d2e05ddef66bd9798d990bcfbd63363ede3736
Juan <juanespinel@gmail.com>
2020-07-06 22:01:11 -0300

    cambie nombre a vista formularioProducto -> products-create-form.ejs y modifique rutas products y productsController

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,15 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+
+let jsonDeProductos = fs.readFileSync(path.join(__dirname, '../data/productsDataBase.json'), 'utf8');
+let productosParseados = JSON.parse(jsonDeProductos);
+
 let productsController = {
-    index: function(req, res, next) {
+  index: function(req, res, next) {
     res.render('todosLosProductos');
   },
-  create: function(req, res, next) {
-    res.render('formularioProducto');
-  },
   detailProduct: function(req, res, next) {
-    res.render('detalleProducto');
+    let idProducto = req.params.productId;
+    for(let i = 0; i < productosParseados.length; i++) {
+      if(productosParseados[i].id == idProducto) {
+        res.render('detalleProducto', {
+          producto: productosParseados[i]
+        })
+      }
+    }
   },
+  // CREAR - muestra formulario vacio para crear
+  create: function(req, res) {
+    res.render('product-create-form');
+  },
+  // CREAR - muestra formulario guardar producto
+  store: function(req, res, next) {
+    let nuevoProducto = {
+      id: Number(productosParseados.length + 1),
+      ...req.body
+    };
+    productosParseados.push(nuevoProducto);
+    let productosActualizados = JSON.stringify(productosParseados);
+    fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), productosActualizados);
+    res.redirect('/products/detail/' + nuevoProdcuto.id)
+  },
+  // MODIFICAR - muestra formulario para editar producto con el producto
   editProduct: function(req, res, next) {
-    res.render('editarProducto');
+    for(let i = 0; i < productosParseados.length; i++) {
+      if (productosParseados[i].id == req.params.productId) {
+        res.render ('product-edit-form', {
+          producto: productosParseados[i]
+        })
+      }
+    }
   },
+  // MODIFICAR - muestra formulario para actualizar producto.
+  updateProduct: (req, res) => {
+    let productoActualizado = {
+      id: Number(req.params.productId),
+      ...req.body
+    };
+    for(let i = 0; i < productosParseados.length; i++) {
+      if (productosParseados[i].id == productoActualizado.id) {
+        productosParseados[i] = productoActualizado;
+        fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), JSON.stringify(productosParseados));
+        res.redirect('/products/detail/' + productoActualizado.id)
+      }
+    }
+  },
+  destroy : (req, res) => {
+		for(let i = 0; i < productosParseados.length; i++) {
+			if(productosParseados[i].id == req.params.productId) {
+				let index = productosParseados.indexOf(productosParseados[i]); productosParseados.splice(index, 1);
+				fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), JSON.stringify(productosParseados));
+				res.redirect('todosLosProductos')
+			}
+		}
+	}
 }
+
 module.exports = productsController;
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,10 +4,18 @@ let productsController = require('../controllers/productsController');
 
 router.get('/', productsController.index);
 // router.post('/', );
+
+router.get('/:productoId', productsController.detailProduct);
+
+//CREAR UN PRODUCTO
 router.get('/create', productsController.create);
-router.get('/:id', productsController.detailProduct);
-router.get('/:id/edit', productsController.editProduct);
-// router.put('/:id', xxxxxxxxxx);
-// router.delete('/:id', xxxxxxxxxx);
+router.post('/create', productsController.store);
+
+//EDITAR UN PRODUCTO
+router.get('/:productoId', productsController.editProduct);
+router.put('/:productoId', productsController.updateProduct);
+
+// ELIMINAR UN PRODUCTO
+router.delete('/delete/:productId', productsController.destroy);
 
 module.exports = router;
