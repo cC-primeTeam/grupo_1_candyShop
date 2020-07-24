690391fa06a5ac24d84b8528e3e37b051181752f
micalewko <mica.lew@hotmail.com>
2020-07-24 16:34:53 -0300

    header y navbar fijos con sombra. Navbar quedo dentro de header. Edicion en todas las vistas.

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+function qs(element) {
+    return document.querySelector(element)
+}
+
+window.addEventListener('load', function() {
+    let header = qs('header');    
+    let offset = qs('#offset');
+    let espacio = 30;
+    offset.style.height = (header.offsetHeight + espacio) + "px";
+    window.addEventListener('resize', function() {
+        offset.style.height = (header.offsetHeight + espacio) + "px";
+    })
+    console.log(header.offsetHeight + "px");
+})
