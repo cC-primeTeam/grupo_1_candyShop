088bcff7582d00fa5980211f49cf19be871d0736
tomiConsek <jtomaschiesa@gmail.com>
2020-08-27 02:51:15 -0300

    Liquidando para la presentacion zoom con Mica y Juan

--- src/database/seeders/090-carritos.js
+++ src/database/seeders/090-carritos.js
@@ -17,7 +17,64 @@ module.exports = {
         updated_at: new Date()
       }, {
         user_id: "2",
-        product_id: "3",
+        product_id: "6",
+        quantity: "2",
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        user_id: "1",
+        product_id: "4",
+        quantity: "5",
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        user_id: "1",
+        product_id: "8",
+        quantity: "9",
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        user_id: "1",
+        product_id: "7",
+        quantity: "2",
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        user_id: "3",
+        product_id: "4",
+        quantity: "5",
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        user_id: "3",
+        product_id: "8",
+        quantity: "9",
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        user_id: "3",
+        product_id: "10",
+        quantity: "5",
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        user_id: "4",
+        product_id: "4",
+        quantity: "5",
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        user_id: "4",
+        product_id: "8",
+        quantity: "9",
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        user_id: "4",
+        product_id: "6",
         quantity: "2",
         created_at: new Date(),
         updated_at: new Date()
